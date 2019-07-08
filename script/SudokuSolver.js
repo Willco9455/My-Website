3f029ae11137c8e3dbaa61ed566e7104429c668c
will <willmoran6969@gmail.com>
2019-07-08 12:01:51 +0100

    commit

--- script/SudokuSolver.js
+++ script/SudokuSolver.js
@@ -2,7 +2,16 @@ function myFunction() {
     var puzzle = {
 
         //this is where the rows are stored when entered -----> in puzzle.rows
-        rows:[[],[],[],[],[],[],[],[],[]],
+        getRows: function(){
+            var rows = [[],[],[],[],[],[],[],[],[]];
+            for(x=0;x<=8;x++){
+                for(y=0;y<=8;y++){
+                    rows[x].push(document.getElementById((String(x)) + (String(y))).value);  
+                }
+            }
+            console.log(rows);
+            return rows;
+        },
 
         //this is the operation insidel the puzzle object that creates the collums ---> accessed with puzzle.getcolums()
         getColums: function(){
@@ -16,16 +25,11 @@ function myFunction() {
             return colums; //returns the full list of colums
         },
         //this is the operation that generates the squares of the puzzle =====
-        squares:[]
-    };
-    for(x=0;x<=8;x++){
-
-        for(y=0;y<=8;y++){
-            puzzle.rows[x].push(document.getElementById((String(x)) + (String(y))).value);
-            console.log(puzzle.getColums());
+        squares: function(){
+            var squares = [[],[],[],[],[],[],[],[],[]];
+            
         }
-
-    }
-
-}
+    };
     
+    console.log(puzzle.getRows())
+}
